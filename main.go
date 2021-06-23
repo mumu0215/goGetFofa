0d5b4991ef6803f3cc28feb025b9f0fa6a025326
mumu0215 <leolin0215@163.com>
2021-06-23 17:55:56 +0800

    fix all

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 	"io/ioutil"
 	"os"
 	"gopkg.in/yaml.v2"
+	"strings"
 )
 
 var(
 	searchStr=flag.String("s","","fofa search string")
-	outFile=flag.String("f","out.txt","output file")
 	json=jsoniter.ConfigCompatibleWithStandardLibrary
 )
 //type result struct {
@@ -61,7 +61,31 @@ func parseResult(result1 []byte) [][]string {
 	//fmt.Println(temp)
 	return temp.Results
 }
-
+func fileOutPut(input [][]string){
+	fileUrl,err:=os.OpenFile("url.txt",os.O_CREATE|os.O_TRUNC|os.O_RDWR,0666)
+	if err!=nil{
+		fmt.Println("fail to open url file")
+		os.Exit(0)
+	}
+	var protocolSlice []string
+	urlAll:=""
+	for _,temp:=range input{
+		tempStr:=""
+		if len(temp[4])!=0{
+			tempStr+=temp[0]+"\t"+temp[4]
+			protocolSlice=append(protocolSlice,tempStr)
+		}else {
+			if strings.HasPrefix(temp[0],"http"){
+				urlAll+=temp[0]+"\r\n"
+			}else {
+				urlAll+="http://"+temp[0]+"\r\n"
+			}
+		}
+	}
+	fileUrl.WriteString(strings.TrimSpace(urlAll))
+	fileUrl.Close()
+	fmt.Println(protocolSlice)
+}
 func main() {
 	flag.Parse()
 	if len(*searchStr)==0{
@@ -76,10 +100,11 @@ func main() {
 		fmt.Printf("create fofa client\n")
 		return
 	}
-	result1, err := clt.QueryAsJSON(1, []byte(*searchStr),[]byte("domain,host,ip,port,title,country,city,protocol"))
+	result1, err := clt.QueryAsJSON(1, []byte(*searchStr),[]byte("host,ip,port,title,protocol,country"))
 	if err != nil {
 		fmt.Printf("%v\n", err.Error())
 	}
 	//fmt.Printf("%s\n", result1)
-	fmt.Println(parseResult(result1))
+	//fmt.Println(parseResult(result1))
+	fileOutPut(parseResult(result1))
 }
